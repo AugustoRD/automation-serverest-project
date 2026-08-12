5fc7b2a1ec1d9c44d1128f5ecdecdf44e18a29ff
AugustoRD <augusto06r@gmail.com>
2026-08-12 11:21:04 -0300

    feat: implement ProdutoBuilder, ProdutoController, and Produto model for product management

---
+++ src/builders/produto.builder.ts
@@ -0,0 +1,39 @@
+import { faker } from "@faker-js/faker";
+import { Produto } from "../models/produtos.model";
+
+export class ProdutoBuilder {
+  private produto: Produto;
+
+  constructor() {
+    this.produto = {
+      nome: faker.commerce.productName(),
+      preco: parseFloat(faker.commerce.price()),
+      descricao: faker.commerce.productDescription(),
+      quantidade: faker.number.int({ min: 1, max: 100 }),
+    };
+  }
+
+  withNome(nome: string) {
+    this.produto.nome = nome;
+    return this;
+  }
+
+  withPreco(preco: number) {
+    this.produto.preco = preco;
+    return this;
+  }
+
+  withDescricao(descricao: string) {
+    this.produto.descricao = descricao;
+    return this;
+  }
+
+  withQuantidade(quantidade: number) {
+    this.produto.quantidade = quantidade;
+    return this;
+  }
+
+  build(): Produto {
+    return this.produto;
+  }
+}
---
+++ src/controllers/produto.controller.ts
@@ -0,0 +1,17 @@
+import { APIRequestContext, APIResponse } from "@playwright/test";
+import { Produto } from "../models/produtos.model";
+
+export class ProdutoController {
+  private request: APIRequestContext;
+  private readonly BASE_URL = "/produtos";
+
+  constructor(request: APIRequestContext) {
+    this.request = request;
+  }
+
+  async criarProduto(payload: Produto): Promise<APIResponse> {
+    return await this.request.post(this.BASE_URL, {
+      data: payload,
+    });
+  }
+}
