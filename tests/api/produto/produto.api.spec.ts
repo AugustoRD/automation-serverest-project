3cd3630c83ed9399f2460cb09269918e94c291fd
AugustoRD <augusto06r@gmail.com>
2026-08-12 14:40:57 -0300

    feat: enhance product creation tests to use admToken and add validation for missing token

--- tests/api/produto/produto.api.spec.ts
+++ tests/api/produto/produto.api.spec.ts
@@ -7,22 +7,71 @@ import { SetupHelper } from "../../../src/helpers/setup.helper";
 test.describe("Produtos API Tests", () => {
   let produtoController: ProdutoController;
   let setupHelper: SetupHelper;
-  let token: string;
+  let admToken: string;
 
   test.beforeEach(async ({ request }) => {
     produtoController = new ProdutoController(request);
     setupHelper = new SetupHelper(request);
 
-    ({ token } = await setupHelper.criarUsuarioELogar("true"));
+    ({ token: admToken } = await setupHelper.criarUsuarioELogar("true"));
   });
 
-  test("should create a new product successfully", async ({ request }) => {
-    const newProduct = new ProdutoBuilder().build();
+  test.describe("Cadastro de produtos", () => {
+    test("should create a new product successfully", async () => {
+      const newProduct = new ProdutoBuilder().build();
 
-    const response = await produtoController.criarProduto(newProduct, token);
-    test.expect(response.status()).toBe(201);
-    test
-      .expect(await response.json())
-      .toHaveProperty("message", "Cadastro realizado com sucesso");
+      const response = await produtoController.criarProduto(
+        newProduct,
+        admToken,
+      );
+      test.expect(response.status()).toBe(201);
+      test
+        .expect(await response.json())
+        .toHaveProperty("message", "Cadastro realizado com sucesso");
+    });
+
+    test("should not create a new product with the same name", async () => {
+      const newProduct = new ProdutoBuilder().build();
+
+      await produtoController.criarProduto(newProduct, admToken);
+
+      const response = await produtoController.criarProduto(
+        newProduct,
+        admToken,
+      );
+      test.expect(response.status()).toBe(400);
+      test
+        .expect(await response.json())
+        .toHaveProperty("message", "Já existe produto com esse nome");
+    });
+
+    test("should not create a new product with a client token", async () => {
+      const { token: clientToken } =
+        await setupHelper.criarUsuarioELogar("false");
+
+      const newProduct = new ProdutoBuilder().build();
+
+      const response = await produtoController.criarProduto(
+        newProduct,
+        clientToken,
+      );
+      test.expect(response.status()).toBe(403);
+      test
+        .expect(await response.json())
+        .toHaveProperty("message", "Rota exclusiva para administradores");
+    });
+
+    test("should not create a new product without a token", async () => {
+      const newProduct = new ProdutoBuilder().build();
+      const response = await produtoController.criarProduto(newProduct, "");
+
+      test.expect(response.status()).toBe(401);
+      test
+        .expect(await response.json())
+        .toHaveProperty(
+          "message",
+          "Token de acesso ausente, inválido, expirado ou usuário do token não existe mais",
+        );
+    });
   });
 });
