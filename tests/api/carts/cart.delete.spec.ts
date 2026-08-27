7752f450e3dd7fb62a081f28540d7d64d9b6d9f0
AugustoRD <augusto06r@gmail.com>
2026-08-27 10:15:28 -0300

    feat: add tests for handling authentication errors and admin privileges in purchase completion

--- tests/api/carts/cart.delete.spec.ts
+++ tests/api/carts/cart.delete.spec.ts
@@ -26,6 +26,34 @@ test.describe("DELETE /carrinhos", () => {
         "Registro excluído com sucesso",
       );
     });
+
+    test("should return an error when trying to complete a purchase without authentication", async ({
+      cartController,
+    }) => {
+      const cartResponse = await cartController.completePurchase("");
+
+      expect(cartResponse.status()).toBe(401);
+      const responseBody = await cartResponse.json();
+      expect(responseBody).toHaveProperty(
+        "message",
+        "Token de acesso ausente, inválido, expirado ou usuário do token não existe mais",
+      );
+    });
+
+    test("should block the purchase completion whith admin privileges", async ({
+      cartController,
+      admToken,
+    }) => {
+      test.fail(
+        true,
+        "This test is expected to fail because the API allows admins to complete purchases, which is not the intended behavior.",
+      );
+      const cartResponse = await cartController.completePurchase(admToken);
+
+      expect(cartResponse.status()).toBe(403);
+      const responseBody = await cartResponse.json();
+      expect(responseBody).toHaveProperty("message", "Acesso negado");
+    });
   });
 
   test.describe("Cancel Purchase", () => {
