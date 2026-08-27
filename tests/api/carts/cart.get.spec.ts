c57bd984eed90c8a95772a111744ed4e4b3a88a5
AugustoRD <augusto06r@gmail.com>
2026-08-27 10:02:08 -0300

    feat: update cart controller and tests to support filtering carts by multiple criteria

--- tests/api/carts/cart.get.spec.ts
+++ tests/api/carts/cart.get.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import { test, expect } from "../../../src/fixtures/api.fixture";
 
 test.describe("GET /carrinhos", () => {
@@ -39,8 +40,8 @@ test.describe("GET /carrinhos", () => {
         [product],
       );
 
-      const cartResponse = await cartController.getCartsByUserId(
-        clientUserId,
+      const cartResponse = await cartController.getCartsByFilters(
+        { idUsuario: clientUserId },
         admToken,
       );
 
@@ -70,6 +71,60 @@ test.describe("GET /carrinhos", () => {
       const responseBody = await cartResponse.json();
 
       expect(responseBody).toHaveProperty("message", "Acesso negado");
+    });
+
+    test("should list carts with multiply filters successfully", async ({
+      setupHelper,
+      cartController,
+      admToken,
+      clientContext: { token: clientToken, id: clientUserId },
+    }) => {
+      const product = await setupHelper.createProduct(admToken);
+      const cartId = await setupHelper.registerCartWithProducts(
+        cartController,
+        clientToken,
+        [product],
+      );
+
+      const expectedPrecoTotal = product.preco.toString();
+
+      const cartResponse = await cartController.getCartsByFilters(
+        { idUsuario: clientUserId, precoTotal: expectedPrecoTotal },
+        admToken,
+      );
+
+      expect(cartResponse.status()).toBe(200);
+      const responseBody = await cartResponse.json();
+
+      expect(responseBody).toHaveProperty("carrinhos");
+      expect(Array.isArray(responseBody.carrinhos)).toBe(true);
+
+      expect(responseBody.carrinhos.length).toBe(1);
+
+      expect(responseBody.carrinhos[0].idUsuario).toBe(clientUserId);
+      expect(responseBody.carrinhos[0].precoTotal).toBe(product.preco);
+    });
+
+    test("should return an empty list when searching for cart with non-existing id", async ({
+      cartController,
+      admToken,
+    }) => {
+      const nonExistingCartId = faker.string.uuid();
+
+      const cartResponse = await cartController.getCartsByFilters(
+        { _id: nonExistingCartId },
+        admToken,
+      );
+
+      expect(cartResponse.status()).toBe(200);
+
+      const responseBody = await cartResponse.json();
+
+      expect(responseBody).toHaveProperty("carrinhos");
+      expect(responseBody).toHaveProperty("quantidade");
+      expect(responseBody.quantidade).toBe(0);
+      expect(Array.isArray(responseBody.carrinhos)).toBe(true);
+      expect(responseBody.carrinhos.length).toBe(0);
     });
   });
 
