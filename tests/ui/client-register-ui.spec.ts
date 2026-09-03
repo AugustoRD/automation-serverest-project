f9c461d8569a89fc470de17a3dfa1eb65eeb0412
AugustoRD <augusto06r@gmail.com>
2026-09-03 10:42:37 -0300

    fix: refactor client registration test to include teardown logic

--- tests/ui/client-register-ui.spec.ts
+++ tests/ui/client-register-ui.spec.ts
@@ -6,12 +6,31 @@ test.describe("Client Register UI Tests", () => {
     await page.goto("/cadastrarusuarios");
   });
 
-  test("should register a new client", async ({ page, clientRegisterPage }) => {
+  test.afterEach(async ({ setupHelper, admToken }) => {
+    await setupHelper.tearDown(admToken);
+  });
+
+  test("should register a new client", async ({
+    page,
+    clientRegisterPage,
+    setupHelper,
+  }) => {
+    const responsePromise = page.waitForResponse(
+      (response) =>
+        response.url().includes("/usuarios") && response.status() === 201,
+    );
+
     await clientRegisterPage.register(
       faker.person.fullName(),
       faker.internet.email(),
       faker.internet.password(),
     );
+
+    const response = await responsePromise;
+    const responseBody = await response.json();
+
+    setupHelper.addUserId(responseBody._id);
+
     await expect(clientRegisterPage.alertSuccessMessage).toBeVisible();
     await expect(page).toHaveURL("/home");
   });
